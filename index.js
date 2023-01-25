81fb686309ec8481e816c1758af8f8db85737e65
OlegGorodenskyi <olgo@owort.com>
2023-01-25 17:39:29 +0200

    changed class Controller and added new functions

--- index.js
+++ index.js
@@ -1,64 +1,103 @@
+window.addEventListener('DOMContentLoaded', () => {
 
-class Model {
-    constructor(boardsize, numShips, shipLength, shipsSunk, ships) {
+    class Model {
+        
         ships = [
             { locations: ['00', '01', '02'], hits: ['', '', ''] },
             { locations: ['20', '21', '22'], hits: ['', '', ''] },
             { locations: ['45', '46', '47'], hits: ['', '', ''] }
         ]
-        this.boardsize = boardsize;
-        this.numShips = numShips;
-        this.shipLength = shipLength;
-        this.shipsSunk = shipsSunk;
-        this.ships = ships;
-    }
+        boardSize = 7;
+        numShips = 3;
+        shipLength = 3;
+        shipsSunk = 0;
+        
+        
+        generateShip() {
+            console.log(this.boardSize)
+            const direction = Math.floor(Math.random() * 2);
+            let row;
+            let col;
+            if (direction == 1) {               /* Horizontal */
+                row = Math.floor(Math.random() * this.boardSize);
+                col = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+            } else {            /* Vertical */
+                row = Math.floor(Math.random() * (this.boardSize - this.shipLength));
+                col = Math.floor(Math.random() * this.boardSize);
+            }
+            let newShipLocations = [];
 
-    boardsize = 7;
-    numShips = 3;
-    shipLength = 3;
-    shipsSunk = 0;
+        }
 
-    fire(guess) {
-        for (let i = 0; i < this.ships.length; i++) {
-            const ship = this.ships[i];
-            const location = ship.locations;
-            let index = location.indexOf(guess);
-            if (index >= 0) {
-                ship.hits[index] = 'hit';
-                view.displayHit(guess);
-                view.displayMessage('HIT');
-                if (this.isSunk(ship)) {
-                    view.displayMessage('You sank my ship');
-                    this.shipsSunk++;
+        fire(guess) {
+            for (let i = 0; i < this.ships.length; i++) {
+                const ship = this.ships[i];
+                const location = ship.locations;
+                let index = location.indexOf(guess);
+                if (index >= 0) {
+                    ship.hits[index] = 'hit';
+                    view.displayHit(guess);
+                    view.displayMessage('HIT');
+                    if (this.isSunk(ship)) {
+                        view.displayMessage('You sank my ship');
+                        this.shipsSunk++;
+                    }
+                    return true;
                 }
-                return true;
+            }
+            view.displayMessage('You missed');
+            view.displayMiss(guess)
+            return false
+        };
+
+        isSunk(ship) {
+            for (let i = 0; i < this.shipLength; i++) {
+                if (ship.hits !== 'hit') {
+                    return false
+                }
+                return true
             }
         }
-        view.displayMessage('You missed');
-        view.displayMiss(guess)
-        return false
-    };
 
-    isSunk(ship) {
-        for (let i = 0; i < this.shipLength; i++) {
-            if (ship.hits !== 'hit') {
-                return false
+    }
+
+
+    class Controller {
+        constructor(guess) {
+            this.guess = guess;
+        }
+        shots = 0;
+
+        processGuess(guess) {
+            let location = parseGuess(guess);
+            if (location) {
+                this.shots++;
+                let hit = play.fire(location);
+                if (hit && this.shipsSunk === this.numShips) {
+                    view.displayMessage(`You sank all Battleships in ${this.shots} shots.`)
+                }
             }
-            return true
         }
     }
 
-}
+    class View extends Model {
+        displayHit(location) {
+            const cell = document.getElementById(location);
+            cell.classList.add('hit');
+        }
 
+        displayMiss(location) {
+            const cell = document.getElementById(location);
+            cell.classList.add('miss');
+        }
 
-class Controller extends Model {
-    constructor(guess, guesses) {
-        super(guess);
-        guesses = 0;
-        this.processGuess();
+        displayMessage(msg) {
+            const messageArea = document.querySelector('#messageArea');
+            messageArea.textContent = msg;
+        }
     }
 
-    parseGuess(guess) {
+    function parseGuess(guess) {
         const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
         if (guess === null || guess.length !== 2) {
             console.log('Should be 2 symbols');
@@ -66,59 +105,57 @@ class Controller extends Model {
             let letter = guess.charAt(0);
             let column = guess.charAt(1);
             let row = alphabet.indexOf(letter);
-            console.log(row)
             if (isNaN(row) || isNaN(column)) {
                 console.log('First symbol should be letter and second - number')
             }
             else if (row < 0 || row >= play.boardSize || column < 0 || column >= play.boardSize) {
                 console.log('Should be from A to G and from 0-6')
-            } else return row + column;
-        }
-        return null;
-    }
-
-    processGuess(guess) {
-        let location = this.parseGuess(guess);
-        if (location) {
-            this.guesses++;
-            let hit = play.fire(location);
-            if (hit && this.shipsSunk === this.numShips) {
-                view.displayMessage(`You sank all Battleships and in ${this.guesses} guesses.`)
+            } else {
+                return row + column;
             }
         }
     }
-}
 
-class View extends Model {
-    // constructor(displayHit, displayMiss, displayMessage,cell) {
-    //     super(displayHit,displayMessage,displayMiss)
-    //     this.cell = cell;
-    //     this.displayHit();
-    //     this.displayMiss();
-    //     this.displayMessage();
-    // }
-    displayHit(location) {
-        const cell = document.getElementById(location);
-        cell.classList.add('hit');
+    function handleFireButton() {
+        const input = document.querySelector('#guessInput');
+        let value = input.value;
+        console.log(value);
+        input.value = '';
     }
 
-    displayMiss(location) {
-        const cell = document.getElementById(location);
-        cell.classList.add('miss');
+    function handleKeyPress(e) {
+        const btn = document.querySelector("#fireButton");
+        if (e.keyCode === 13) {
+            btn.click();
+            return false;
+        }
     }
 
-    displayMessage(msg) {
-        const messageArea = document.querySelector('#messageArea');
-        messageArea.textContent = msg;
+    function start() {
+        const btn = document.querySelector("#fireButton");
+        btn.onclick = handleFireButton;
+
+        const input = document.querySelector('#guessInput');
+        input.onkeypress = handleKeyPress;
     }
-}
 
-const view = new View();
-const play = new Model();
-const div = new Controller();
-div.processGuess('A0')
+    start();
 
-// controller.processGuess("A0"); controller.processGuess("A6"); controller.processGuess("B6"); controller.processGuess("C6"); controller.processGuess("C4"); controller.processGuess("D4"); controller.processGuess("E4"); controller.processGuess("B0"); controller.processGuess("B1"); controller.processGuess("B2");
+    const play = new Model();
+    const view = new View();
+    const div = new Controller('A0');
+    // play.generateShip();
+    div.processGuess('E5')
+    div.processGuess('E3')
+    div.processGuess('E2')
+    div.processGuess('E1')
+    div.processGuess('E0')
+    div.processGuess('E6')
+    div.processGuess('E7')
+    div.processGuess('D0')
+
+});
+
 
 
 
